410b42a3b69b7f619ecebcbbab22aab5a518cf28
晒晕的兔子 <gubu1986@126.com>
2022-12-17 21:47:36 +0800

    Initial Commit

---
+++ HttpDog/HttpDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HttpDogApp.swift
+//  HttpDog
+//
+//  Created by 顾艳华 on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct HttpDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
